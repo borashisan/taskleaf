4632ec0e9012d7d05db46649810abe234b8972a9
borashisan <tomochuro731@gmail.com>
2022-06-03 15:40:21 +0900

    flashメッセージを表示できるように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_29_075929) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_213701) do
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
